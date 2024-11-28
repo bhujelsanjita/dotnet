dbc00d27359b0af6a55c1006d3a5bb1f2e092a5a
Sanjita Bhujel <bhujelsanjita7@gmail.com>
2024-11-28 09:32:06 +0545

    Method overlaoding class

---
+++ methodoverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Method_Overlaoading
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Add(float a, float b)
+        {
+            return Convert.ToInt32(a + b);
+        }
+        public float Add(float a, float b, float c)
+        {
+            return a + b + c;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main()
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("Add(int,int):" + calc.Add(6, 6));
+            Console.WriteLine("Add(int,int,int):" + calc.Add(6, 6, 5.1));
+            Console.WriteLine("Add(double,double):" + calc.Add(6.3, 8.5));
+        }
+    }
+}
